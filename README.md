4d7e0802064b5233114a5c0637f63ddc405dcd94
Lasse Bjermeland <lasse.bjermeland@sintef.no>
2023-01-25 08:49:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# SFIBlues Metocean library
+
+Illustrates fetching Metocean data from different sources and then demonstrate several usages. 
+Developed for use with the  [SFI Blues Metocean python library](https://github.com/SINTEF/blues-metocean-lib)
+
+Necessary dependencies installed using
+
+pip install -r requirements.txt
---
+++ src/normet/nora3.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from bluesmet.normet.nora3 import wave_sub_time, arome3km_3hr
+
+# Coordinates we want to get data for
+lat_pos = 64.731729
+lon_pos = 5.835813
+start_date = datetime(2020, 10, 21)
+end_date = datetime(2020, 11, 21)
+
+requested_values = [
+    "hs",
+    "tp"
+]
+values = wave_sub_time.get_values_between(
+    lat_pos, lon_pos, start_date, end_date, requested_values
+)
+
+# Coordinates we actually to get data for (nearest grid point)
+alat = values["latitude_actual"]
+alon = values["longitude_actual"]
+print(f"Actual coordinates: {alat} lon: {alon}")
+
+for requested_value in requested_values:
+    print(f"{requested_value}_mean: {values[requested_value].mean()}")
+
+requested_values = ["wind_speed", "wind_direction"]
+values = arome3km_3hr.get_values_between(
+    lat_pos, lon_pos, start_date, end_date, requested_values
+)
+alat = values["latitude_actual"]
+alon = values["longitude_actual"]
+print(f"Actual coordinates 2: {alat} lon: {alon}")
+for requested_value in requested_values:
+    print(f"{requested_value}_mean: {values[requested_value].mean()}")
